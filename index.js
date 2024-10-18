b307bcbda464bea413c55cc7dced1817dac30ef0
R-Swetha1103 <swetharagupathy1103@gmail.com>
2024-10-18 22:58:35 +0530

    add frontend link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Change this to your frontend URL
+    origin: 'https://thriving-otter-5e3237.netlify.app/', // Change this to your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods you want to allow
     credentials: true // if you need cookies on cross-origin requests
 };
